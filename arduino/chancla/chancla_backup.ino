28d920b7cdf2b735b3d2da635d8483d639aeb255
ekiMGlz <mike.glz.96@gmail.com>
2019-05-22 08:10:03 -0500

    update a la chancla

--- arduino/chancla/chancla_backup.ino
+++ arduino/chancla/chancla_backup.ino
@@ -2,35 +2,31 @@ int encoderDer = 42;
 int encoderIzq = 44;
 int counterEncDer = 0;
 int counterEncIzq = 0;
-unsigned long t0 = 0;
+unsigned long t0=0;
 
 int encDerVal = 0;
 int encIzqVal = 0;
 
 int currVal = 0;
 
-// Motor 1 es der
+//Motor 1 es der
 int motor1_red = 11;
 int motor1_white = 12;
 
-// Motor 2 es izq
+//Motor 2 es izq
 int motor2_red = 5;
 int motor2_white = 2;
 
-// Velocidad en encoder ticks/seg
-double targetR = 0;
-double targetL = 0;
+int targetR = 0;
+int targetL = 0;
 
-// Cada cuanto revizar velocidad (en milis)
-int sample_time = 100;
+double kpr = 0.6;
+double kir = 0.6;
+double kdr = 0.1;
 
-double kpr = 1.2;
-double kir = 1.2;
-double kdr = 0.2;
-
-double kpl = 1;
-double kil = 1.4;
-double kdl = 0.2;
+double kpl = 0.5;
+double kil = 0.7;
+double kdl = 0.1;
 
 double err_r = 0;
 double sum_err_r = 0;
@@ -70,27 +66,31 @@ void setup(){
 }
 
 void loop(){
-
-  // Contar encoder derecho
+  
   currVal = digitalRead(encoderDer);
+  
   if(encDerVal == HIGH){
     if(currVal == LOW){
+      encDerVal = currVal;
       counterEncDer++;
     }
   }
+  
   encDerVal = currVal;
 
-  // Contar encoder izquierdo
   currVal = digitalRead(encoderIzq);
+  
   if(encIzqVal == HIGH){
     if(currVal == LOW){
+      encIzqVal = currVal;
       counterEncIzq++;
     }
   }
+  
   encIzqVal = currVal;
   
-  // Cada sample_time ms calcula velocidad y PID
-  if(millis() >= t0 + sample_time){
+  // Cada 500 ms resetea
+  if(millis() >= t0 + 500){
     
     Serial.print("\nenc derecho\n");
     Serial.print(counterEncDer);
@@ -99,28 +99,22 @@ void loop(){
     Serial.print(counterEncIzq);
     
     //PID
+    err_r = targetR - counterEncDer;
+    err_l = targetL - counterEncIzq;
     
-    // Error actual es target - velocidad en ticks/segundo
-    err_r = targetR - (sample_time/1000)*counterEncDer;
-    err_l = targetL - (sample_time/1000)*counterEncIzq;
-
-    // Error integral
     sum_err_r += err_r;
     sum_err_l += err_l;
-
-    // PID completo
+    
     pid_r = err_r*kpr + (err_r - prev_err_r)*kdr + sum_err_r*kir;
     pid_l = err_l*kpl + (err_l - prev_err_l)*kdl + sum_err_l*kil;
-
-    // Actualizar error previo
+    
     prev_err_r = err_r;
     prev_err_l = err_l;
-
-    // Ajustar salida
+    
     out_r = min(abs(pid_r), 255);
     out_l = min(abs(pid_l), 255);
     
-    // Por ahora, solo hay targets positivos.
+    
     if (pid_r >= 0){
       analogWrite(motor1_red, out_r);
       analogWrite(motor1_white, 0);
@@ -129,7 +123,7 @@ void loop(){
       analogWrite(motor1_white, out_r);
     }
     
-    // Por ahora, solo hay targets positivos
+    
     if (pid_l >= 0){
       analogWrite(motor2_red, out_l);
       analogWrite(motor2_white, 0);
@@ -137,8 +131,7 @@ void loop(){
       analogWrite(motor2_red, 0);
       analogWrite(motor2_white, out_l);
     }
-
-    // Volver a empezar tiempo para calcular velocidad
+    
     t0 = millis();
     counterEncDer = 0;
     counterEncIzq = 0;
